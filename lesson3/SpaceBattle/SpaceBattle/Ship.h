34815316e68c6f56fce249c9acfddb78d91b4ce7
bryant <bryanttgm92778@gmail.com>
2015-09-17 20:20:26 +0800

    add Class

---
+++ lesson3/SpaceBattle/SpaceBattle/Ship.h
@@ -0,0 +1,13 @@
+//
+//  Ship.h
+//  SpaceBattle
+//
+//  Created by bryant on 17/9/15.
+//  Copyright (c) 2015 cpttm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ship : NSObject
+
+@end
---
+++ lesson3/SpaceBattle/SpaceBattle/Ship.m
@@ -0,0 +1,13 @@
+//
+//  Ship.m
+//  SpaceBattle
+//
+//  Created by bryant on 17/9/15.
+//  Copyright (c) 2015 cpttm. All rights reserved.
+//
+
+#import "Ship.h"
+
+@implementation Ship
+
+@end
