fe98a7f0775ff1fc8c29c5104acd9f65bc926430
bryant <bryanttgm92778@gmail.com>
2015-09-22 21:51:29 +0800

    upload picker view

---
+++ lesson4/helloPickview/helloPickview/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  helloPickview
+//
+//  Created by bryant on 22/9/15.
+//  Copyright © 2015 cpttm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
+{
+
+    NSArray *options;
+    
+    NSArray *myfirends;
+}
+
+
+
+
+@end
+
